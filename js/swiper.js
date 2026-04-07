94827f6a0552bcc053a755d9b49b0ec922003f5d
rainwhdmsql-stack <rainwhdmsql@gmail.com>
2026-04-07 18:10:54 +0900

    메인 half 작업 완료

--- js/swiper.js
+++ js/swiper.js
@@ -1,7 +1,8 @@
 var swiper = new Swiper(".Pr-mySwiper", {
-  slidesPerView: 1,
+  slidesPerView: 1.5,
   spaceBetween: 30,
   loop: true,
+  centeredSlides: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
